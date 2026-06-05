09643770d06447d9dd1864075c44ea9e6f8d15b2
Gaurav-Popli <gaurav.popli018@svkmmumbai.onmicrosoft.com>
2026-06-05 12:40:08 +0000

    comparison part1 done

---
+++ Day4/comparison.js
@@ -0,0 +1,18 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);//string 2 is automatically converted into number then performs comparison and gives answer as true or false (here ans is "true")
+// console.log(null>0);
+// console.log(null>=0);//returns true as JS >=converts null into 0 (or sometimes into NaN)and thenperforms comparison and //these are confusing conversions
+// console.log(null==0); //here i guess it directly compares and //these are confusing conversions
+// console.log(null<=0);//returns true as JS >=converts null into 0 and then performs comparison and //these are confusing conversions
+console.log(undefined>0);//these are confusing conversions
+console.log(undefined<0);//these are confusing conversions
+console.log(undefined==0);//these are confusing conversions
+
+//strict check(===) checks value as well as datatypes
+console.log("2"===2); //returns false since value is same but datatype is different
