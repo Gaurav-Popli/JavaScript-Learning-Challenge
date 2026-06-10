c004771b9de2fcec854ff50a23513b0d4d6aa741
Gaurav-Popli <gaurav.popli018@svkmmumbai.onmicrosoft.com>
2026-06-10 21:55:22 +0530

    object part2

--- Day8/object1.js
+++ Day8/object1.js
@@ -27,4 +27,21 @@ const obj2={3:"c",4:"d"};
 const obj3={...obj1,...obj2}//using spread operator to merge two objects into one object i.e.in name as obj3
 console.log(obj3)
 
+const users=[
+    {id:1,
+        email:"123@abc.com",
+    },
+    {id:2,
+        email:"223@abc.com",
+    },
+    {id:3,
+        email:"323@abc.com",
+    }
+]
+console.log(users[1].email)
+console.log(instaUser1)
+console.log(Object.keys(instaUser1))//gives array of keys of the object
+console.log(Object.values(instaUser1))//gives array of values of the object
+console.log(Object.entries(instaUser1))//gives array of key value pairs of the object
 
+console.log(instaUser1.hasOwnProperty("isLoggedIn"))//gives true if the object has the property mentioned in the parameter otherwise gives false
