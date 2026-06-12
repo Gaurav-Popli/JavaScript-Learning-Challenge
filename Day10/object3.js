184a5565a21d5232d51fe35604408de5b13688e6
Gaurav-Popli <gaurav.popli018@svkmmumbai.onmicrosoft.com>
2026-06-12 08:06:23 +0530

    Create object3.js

---
+++ Day10/object3.js
@@ -0,0 +1,30 @@
+const course={
+    courseName:"JavaScript",
+    price:1000,
+    courseInstructor:"Gaurav",
+}
+//course.courseInstructor
+const {courseInstructor}=course;//object destructuring
+const {courseInstructor:instructor}=course;//object destructuring
+
+console.log(courseInstructor)
+console.log(instructor)
+// //destructuring object in function parameter part of react language
+// const navbar=({company})=>{
+
+// }
+// navbar(company="Gaurav")
+
+
+//API
+// {
+//     "name":"Gaurav",
+//      "price":"1000",
+//     "courseInstructor":"Gaurav",
+// }
+
+[
+    {},
+    {},
+    {},
+]
